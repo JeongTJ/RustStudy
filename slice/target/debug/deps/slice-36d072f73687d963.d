d32bf684c3c35b5bb3c2f2918a9ef20ba58211a6
정태준 <jeongtaejun@jeongtaejun-ui-noteubug.local>
2024-03-05 12:43:38 +0900

    slice: hmm... idk

---
+++ slice/target/debug/deps/slice-36d072f73687d963.d
@@ -0,0 +1,5 @@
+/Users/jeongtaejun/Documents/develop/rust_study/slice/target/debug/deps/slice-36d072f73687d963: src/main.rs
+
+/Users/jeongtaejun/Documents/develop/rust_study/slice/target/debug/deps/slice-36d072f73687d963.d: src/main.rs
+
+src/main.rs:
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+/Users/jeongtaejun/Documents/develop/rust_study/slice/target/debug/slice: /Users/jeongtaejun/Documents/develop/rust_study/slice/src/main.rs
